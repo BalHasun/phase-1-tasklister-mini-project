a63725ab0eb8e06b84b5ddd5739026ed650341bb
Bal Elsada Hasun <fouadhason@gmail.com>
2023-04-24 12:11:15 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const sumBtn = document.forms[0].elements[1];
+
+  sumBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    let textinfo = document.forms[0].elements[0].value;
+    const todo = document.querySelector('#tasks')
+    const list = document.createElement('li')
+    list.textContent = textinfo
+    todo.appendChild(list)
+  })
+  console.log(sumBtn)
 });
+
